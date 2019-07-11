e82de3f6c9f4e2536ce239679948fa583c7c5e7d
Will Farrell <wfarrell@1904labs.com>
2019-07-11 16:54:15 -0500

    Define a slack message model. Parse the dataframe down into the a simplified slack message.

---
+++ src/main/scala/com/farrellw/models/SlackMessage.scala
@@ -0,0 +1,3 @@
+package com.farrellw.models
+
+case class SlackMessage(text: String, mood: Option[String], innovationHours: Option[Boolean])
--- src/main/scala/com/farrellw/spark/MyApp.scala
+++ src/main/scala/com/farrellw/spark/MyApp.scala
@@ -1,19 +1,30 @@
 package com.farrellw.spark
 
 import org.apache.spark.sql.SparkSession
+import com.farrellw.models.SlackMessage
+
 
 object MyApp {
   val applicationName = "SlackAnalysis"
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName(applicationName).master("local[1]").getOrCreate()
+
+    import spark.implicits._
+
     val sc = spark.sparkContext
 
     val path = "/Users/will.farrell/dev/1904/spark-streaming/src/main/resources/test_messages.json"
     val multilineOption = true
 
-    val messagesDF = spark.read.option("multiline",multilineOption).json(path)
+    val messagesDF = spark.read.option("multiline", multilineOption).json(path)
 
-    messagesDF.printSchema()
+    print("***********************************")
+    val parsedDF = messagesDF.map(row => {
+      val text = row.getAs[String]("text")
+      SlackMessage(text, None, None)
+    })
+
+    parsedDF.show(5)
   }
 }
